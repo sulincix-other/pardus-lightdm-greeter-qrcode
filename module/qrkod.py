1a7e3683fa93cb0906eccc18bfbc9fc777e3cb25
sulincix <aliriza.keskin@pardus.org.tr>
2023-01-12 08:34:17 +0000

    Update qrkod.py

--- module/qrkod.py
+++ module/qrkod.py
@@ -44,6 +44,7 @@ def module_init():
     button = Gtk.MenuButton(label="QR", popover=qrpopover)
     button.connect("clicked",_qrkod_button_event)
     loginwindow.o("ui_box_bottom_right").pack_end(button, False, True, 10)
+    button.get_style_context().add_class("icon")
     button.show_all()
     qrimage.show()
     update_qr_image()
